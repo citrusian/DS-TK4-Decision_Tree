cdc5ba6a78b0fb6ee4d1b66ac77a9e5632447995
citrusian <115960900+citrusian@users.noreply.github.com>
2023-07-31 01:38:12 +0700

    Modify into 2d Decision Tree

--- main.py
+++ main.py
@@ -181,57 +181,25 @@ for attribute in attributes:
 # License: BSD 3 clause
 
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import datasets, tree
 
-# unused but required import for doing 3d projections with matplotlib < 3.2
-import mpl_toolkits.mplot3d  # noqa: F401
-
-from sklearn import datasets
-from sklearn.decomposition import PCA
-
-# import some data to play with
+# Change to use data from panda
 iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features.
-y = iris.target
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['target'] = iris.target_names[iris.target]
 
-x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
-y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
 
-plt.figure(2, figsize=(8, 6))
-plt.clf()
+X = df.drop('target', axis=1)
+y = df['target']
 
-# Plot the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor="k")
-plt.xlabel("Sepal length")
-plt.ylabel("Sepal width")
+model = tree.DecisionTreeClassifier()
+model.fit(X, y)
 
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
-
-# To getter a better understanding of interaction of the dimensions
-# plot the first three PCA dimensions
-fig = plt.figure(1, figsize=(8, 6))
-ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
-
-X_reduced = PCA(n_components=3).fit_transform(iris.data)
-ax.scatter(
-  X_reduced[:, 0],
-  X_reduced[:, 1],
-  X_reduced[:, 2],
-  c=y,
-  cmap=plt.cm.Set1,
-  edgecolor="k",
-  s=40,
-)
-
-ax.set_title("First three PCA directions")
-ax.set_xlabel("1st eigenvector")
-ax.xaxis.set_ticklabels([])
-ax.set_ylabel("2nd eigenvector")
-ax.yaxis.set_ticklabels([])
-ax.set_zlabel("3rd eigenvector")
-ax.zaxis.set_ticklabels([])
+# Output decision tree
+plt.figure(figsize=(15, 10))
+tree.plot_tree(model, feature_names=list(X.columns), class_names=list(iris.target_names), filled=True)
+plt.show()
 
 plt.show()
 
